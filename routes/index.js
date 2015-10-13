350ad095f7a857471f504de26e97b755a1472ed4
TimHarasym91 <timharasym@gmail.com>
2015-10-13 15:13:12 -0400

    Formatting & Page setup
    Added some formatting, and CSS. Changed the layout a bit and added set
    up for adding content to pages.

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', message: 'the home page'});
+  res.render('index', { title: 'Home', message: 'Welcome to my website.'});
 });
 
 
 /* GET About page */
 router.get('/about', function(req, res, next) {
-	res.render('about');
+	res.render('about', { title: 'About', message: 'All about me.'});
 });
 
 /* GET Projects page */
 router.get('/projects', function(req, res, next) {
-	res.render('projects');
+	res.render('projects', { title: 'Projects', message: 'Check out some of my projects below.'});
 });
 
 /* GET Services page */
 router.get('/services', function(req, res, next) {
-	res.render('services');
+	res.render('services', { title: 'Services', message: 'See what skills and services I have to offer.'});
 });
 
 /* GET Contact Me page */
 router.get('/contact', function(req, res, next) {
-	res.render('contact');
+	res.render('contact', { title: 'Contact', message: 'Feel free to contact me anytime, and I\'ll get back to you ASAP.'});
 });
 
 module.exports = router;
